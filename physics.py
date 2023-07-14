528c13eb8408e5c62bba30dc90d05707dca38fcc
SpyguyOO7 <wgao2005007@gmail.com>
2023-07-14 14:34:33 -0400

    finished physics file

--- physics.py
+++ physics.py
@@ -38,8 +38,86 @@ def calculate_angular_acceleration(tau, I):
 
 
 def calculate_torque(F_magnitude, F_direction, r):
-    return r * F_magnitude * np.sin(F_magnitude)
+    return r * F_magnitude * np.sin(np.radians(F_direction))
 
 
 def calculate_moment_of_inertia(m, r):
-    return m * (r ^ 2)
+    return m * r * r
+
+
+def calculate_auv_acceleration(
+    F_magnitude, F_angle, mass=100, volume=0.1, thruster_distance=0.5
+):
+    return calculate_acceleration(F_magnitude, mass)
+
+
+def calculate_auv_angular_acceleration(
+    F_magnitude, F_angle, inertia=1, thruster_distance=0.5
+):
+    torque = calculate_torque(F_magnitude, F_angle, thruster_distance)
+    # return inertia
+    # moment_of_inertia = calculate_moment_of_inertia(mass, thruster_distance)
+    return calculate_angular_acceleration(torque, inertia)
+
+
+def calculate_auv2_acceleration(T, alpha, theta, mass=100):
+    # Force = np.array(shape=(2, 1))
+    cosAngle = np.cos(alpha)
+    sinAngle = np.sin(alpha)
+    # ThrusterDirections = np.empty([2, 2], dtype=float)
+    ThrusterDirections = np.array(
+        [
+            cosAngle,
+            cosAngle,
+            -cosAngle,
+            -cosAngle,
+            sinAngle,
+            -sinAngle,
+            -sinAngle,
+            sinAngle,
+        ]
+    ).reshape(2, 4)
+    RobotCos = np.cos(theta)
+    RobotSin = np.sin(theta)
+    RotationMatrix = np.array(
+        [
+            RobotCos,
+            -RobotSin,
+            RobotSin,
+            RobotCos,
+        ]
+    ).reshape(2, 2)
+    # ThrusterDirections = np.reshape
+    # print(ThrusterDirections)
+    # print(T)
+    LocalForce = np.dot(ThrusterDirections, T)
+
+    RobotForce = np.dot(RotationMatrix, LocalForce)
+    Acceleration = np.divide(RobotForce, mass)
+    # print(Force)
+    # print(Acceleraton)
+    return Acceleration
+
+
+test = np.array([2, 2, 1, 1]).reshape(4, 1)
+print(calculate_auv2_acceleration(test, 45, 45))
+
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia=100):
+    r = np.sqrt(np.power(L, 2) + np.power(l, 2))
+    TotalTorque = 0
+    counter = 0
+    for i in T:
+        # print(i)
+        if counter % 2 == 0:
+            TotalTorque += T[counter] * (np.sin(alpha) * L + np.cos(alpha) * l)
+        else:
+            TotalTorque -= T[counter] * (np.sin(alpha) * L + np.cos(alpha) * l)
+        counter += 1
+    # print(TotalTorque)
+    return calculate_angular_acceleration(TotalTorque, inertia)
+
+
+test = np.array([1, 3, 1, 2])
+print(calculate_auv2_angular_acceleration(test, 0.5, 1.5, 1.8))
+print(calculate_auv2_acceleration(test, 0.5, 0.3))
--- test_physics.py
+++ test_physics.py
@@ -1,28 +1,55 @@
 import unittest
 import physics
+import math
+
 
 class TestPhysics(unittest.TestCase):
     def test_calculate_buoyancy(self):
-        self.assertEqual(physics.calculate_buoyancy(3,2), 6*9.81)
-        self.assertNotEqual(physics.calculate_buoyancy(4,2), 6*9.81)
+        self.assertEqual(physics.calculate_buoyancy(3, 2), 6 * 9.81)
+        self.assertNotEqual(physics.calculate_buoyancy(4, 2), 6 * 9.81)
         with self.assertRaises(ValueError):
-            physics.calculate_buoyancy(-1,0)
+            physics.calculate_buoyancy(-1, 0)
         with self.assertRaises(ValueError):
-            physics.calculate_buoyancy(1,-1)
+            physics.calculate_buoyancy(1, -1)
 
     def test_will_it_float(self):
-        self.assertEqual(physics.will_it_float(1,1), True)
-        self.assertEqual(physics.will_it_float(1,1001), False)
+        self.assertEqual(physics.will_it_float(1, 1), True)
+        self.assertEqual(physics.will_it_float(1, 1001), False)
         with self.assertRaises(ValueError):
-            physics.will_it_float(-1,0)
+            physics.will_it_float(-1, 0)
         with self.assertRaises(ValueError):
-            physics.will_it_float(1,-1)
+            physics.will_it_float(1, -1)
 
     def test_calculate_pressure(self):
-        self.assertEqual(physics.calculate_pressure(20), 1000*9.81*20+101325)
-        self.assertEqual(physics.calculate_pressure(10), 1000*9.81*10+101325)
+        self.assertEqual(physics.calculate_pressure(20), 1000 * 9.81 * 20 + 101325)
+        self.assertEqual(physics.calculate_pressure(10), 1000 * 9.81 * 10 + 101325)
         with self.assertRaises(ValueError):
             physics.calculate_pressure(-1)
+
+    def test_calculate_acceleration(self):
+        self.assertEqual(physics.calculate_acceleration(6, 2), 3)
+        self.assertNotEqual(physics.calculate_acceleration(6, 2), 0)
+
+    def test_calculate_angular_acceleration(self):
+        self.assertEqual(physics.calculate_angular_acceleration(6, 2), 3)
+        self.assertNotEqual(physics.calculate_angular_acceleration(6, 2), 0)
+
+    def test_calculate_torque(self):
+        self.assertEqual(physics.calculate_torque(6, 90, 2), 12)
+        self.assertNotEqual(physics.calculate_torque(6, 90, 2), 0)
+
+    def test_calculate_moment_of_inertia(self):
+        self.assertEqual(physics.calculate_moment_of_inertia(6, 2), 24)
+        self.assertNotEqual(physics.calculate_moment_of_inertia(6, 90), 0)
+
+    def test_calculate_auv_acceleration(self):
+        self.assertEqual(physics.calculate_auv_acceleration(6, 2, 2), 3)
+        self.assertNotEqual(physics.calculate_auv_acceleration(6, 2, 2), 0)
+
+    def test_calculate_auv_angular_acceleration(self):
+        self.assertEqual(physics.calculate_auv_angular_acceleration(6, 90, 2, 2), 6)
+        self.assertNotEqual(physics.calculate_auv_angular_acceleration(6, 2, 2, 2), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
